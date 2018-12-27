dd80e19655617cd3ff9c6fed7f7ef1a0c3a03e01
chengzh2008 <>
2018-12-26 17:47:30 -0800

    add login page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,26 @@
 import * as React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import ProductPage from "./ProductPage";
 import Header from "./Header";
 import NotFoundPage from "./NotFoundPage";
+import LoginPage from "./LoginPage";
 
 const Routes: React.SFC = () => {
+    const [loggedIn, setLoggedIn] = React.useState(false);
     return (
         <Router>
             <div>
                 <Header />
                 <Switch>
+                    <Redirect exact={true} from="/" to="/products" />
                     <Route exact={true} path="/products" component={ProductsPage} />
                     <Route path="/products/:id" component={ProductPage} />
-                    <Route path="/admin" component={AdminPage} />
+                    <Route path="/admin">
+                        {loggedIn ? <AdminPage /> : <Redirect to="/login" />}</Route>
+                    <Route path="/login" component={LoginPage} />
                     <Route component={NotFoundPage} />
                 </Switch>  
             </div>

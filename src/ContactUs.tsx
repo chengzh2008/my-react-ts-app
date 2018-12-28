7ff7777d725d85e70a591640095122c78b0dd0ab
chengzh2008 <>
2018-12-28 12:14:22 -0800

    refactor the form to be generic

--- src/ContactUs.tsx
+++ src/ContactUs.tsx
@@ -1,64 +1,20 @@
 import * as React from "react";
 
-interface IProps {
-  name: string;
-  email: string;
-  reason: string;
-  notes: string;
-  onNameChange: (name: string) => void;
-  onEmailChange: (name: string) => void;
-  onReasonChange: (name: string) => void;
-  onNotesChange: (name: string) => void;
-}
+import { Form } from "./Form";
 
-const ContactUs: React.SFC<IProps> = props => {
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    props.onNameChange(e.currentTarget.value);
-  };
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    props.onEmailChange(e.currentTarget.value);
-  };
-  const handleNotesChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    props.onNotesChange(e.currentTarget.value);
-  };
-  const handleReasonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    props.onReasonChange(e.currentTarget.value);
-  };
+const ContactUs: React.SFC = () => {
   return (
-    <form className="form" noValidate={true}>
-      <div className="form-group">
-        <label htmlFor="name">Your name</label>
-        <input
-          type="text"
-          id="name"
-          onChange={handleNameChange}
-          value={props.name}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Your email</label>
-        <input
-          type="text"
-          id="email"
-          onChange={handleEmailChange}
-          value={props.email}
-        />
-      </div>
-      <div className="form-group">
-     <label htmlFor="reason">Reason you need to contact us</label>
-     <select id="reason" value={props.reason} onChange={handleReasonChange}>
-       <option value="Marketing">Marketing</option>
-       <option value="Support">Support</option>
-       <option value="Feedback">Feedback</option>
-       <option value="Jobs">Jobs</option>
-       <option value="Other">Other</option>
-    </select>
-  </div>
-      <div className="form-group">
-    <label htmlFor="notes">Additional notes</label>
-    <textarea id="notes" value={props.notes} onChange={handleNotesChange} />
-  </div>
-    </form>
+    <Form defaultValues={{ name: "", email: "", reason: "Support", notes: "" }}>
+      <Form.Field name="name" label="Your name" />
+      <Form.Field name="email" label="Your email address" type="Email" />
+      <Form.Field
+        name="reason"
+        label="Reason you need to contact us"
+        type="Select"
+        options={["Marketing", "Support", "Feedback", "Jobs", "Other"]}
+      />
+      <Form.Field name="notes" label="Additional notes" type="TextArea" />
+    </Form>
   );
 };
 
--- src/ContactUsPage.tsx
+++ src/ContactUsPage.tsx
@@ -1,62 +1,19 @@
 import * as React from "react";
 import ContactUs from "./ContactUs";
 
+class ContactUsPage extends React.Component {
 
-interface IState {
-    name: string;
-    email: string;
-    reason: string;
-    notes: string;
-}
-
-class ContactUsPage extends React.Component<{}, IState> {
-
-    public constructor(props: {}) {
-        super(props);
-        this.state = {
-            email: "",
-            name: "",
-            notes: "",
-            reason: ""
-        };
-    }
-
-    public render() {
-        return (
-            <div className="page-container">
-                <h1>Contact Us</h1>
-                <p>
-                    If you enter your details we will get back to you as soon as possible.
-                </p>
-                <ContactUs 
-                    name={this.state.name}
-                    onNameChange={this.onNameChange}
-                    email={this.state.email}
-                    onEmailChange={this.onEmailChange}
-                    notes={this.state.notes}
-                    onNotesChange={this.onNotesChange}
-                    reason={this.state.reason}
-                    onReasonChange={this.onReasonChange}
-                />
-            </div>
-        );
-    }
-
-    private onNameChange = (name: string) => {
-        this.setState({name});
-    }
-
-    private onEmailChange = (email: string) => {
-        this.setState({email});
-    }
-
-    private onNotesChange = (notes: string) => {
-        this.setState({notes});
-    }
-
-    private onReasonChange = (reason: string) => {
-        this.setState({reason});
-    }
+  public render() {
+    return (
+      <div className="page-container">
+        <h1>Contact Us</h1>
+        <p>
+          If you enter your details we will get back to you as soon as possible.
+        </p>
+        <ContactUs />
+      </div>
+    );
+  }
 }
 
 export default ContactUsPage;

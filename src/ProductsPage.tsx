fbde1f55b2bb995fe5317c15672450afdeaaa123
chengzh2008 <>
2018-12-29 16:58:53 -0800

    save temp

--- src/ProductsPage.tsx
+++ src/ProductsPage.tsx
@@ -1,64 +1,69 @@
 import * as React from "react";
-import {Link, RouteComponentProps} from "react-router-dom";
-import {IProduct, products} from "./ProductsData";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
 
-interface IState {
+import { IProduct } from "./ProductsData";
+import { IApplicationState } from "./Store";
+import { getProducts } from "./ProductsData";
+
+interface IProps extends RouteComponentProps {
+    getProducts: typeof getProducts;
+    loading: boolean;
     products: IProduct[];
-    search: string;
 }
 
-class ProductsPage extends React.Component<RouteComponentProps, IState> {
-    
-    public static getDerivedStateFromProps(
-        props: RouteComponentProps,
-        state: IState
-    ) {
-        const searchParams = new URLSearchParams(props.location.search);
-        const search = searchParams.get("search") || "";
-        return {
-            products: state.products,
-            search
-        };
-    }
-    
-    public constructor(props: RouteComponentProps) {
-        super(props);
-        this.state = {
-            products: [],
-            search: ""
-        };
-    }
+class ProductsPage extends React.Component<IProps> {
 
-    public componentDidMount() {
-        this.setState({products});
-    }
-
-    public render() {
-        return (
-            <div className="page-container">
-                <p>
-                    Welcome to React Shop where you can get all your tools for ReactJS!
-                </p>
-                <ul className="product-list">
-                    {this.state.products.map(product => {
-                        if (!this.state.search || 
-                            (this.state.search && 
-                                product.name.toLocaleLowerCase()
-                                .indexOf(this.state.search.toLocaleLowerCase()) > -1)) {
-                                    return <li key={product.id} className="product-list-item">
-                                    <Link to={`/products/${product.id}`}>
-                                        {product.name}
-                                    </Link>
-                                </li>;
-                        } else {
-                            return null;
-                        }
-                    })}
-                </ul>
-            </div>
-            
-        )
-    }
+  public componentDidMount() {
+    this.props.getProducts();
 }
 
-export default ProductsPage;
+  public render() {
+      const searchParams = new URLSearchParams(this.props.location.search);
+      const search = searchParams.get("search") || "";
+    return (
+      <div className="page-container">
+        <p>
+          Welcome to React Shop where you can get all your tools for ReactJS!
+        </p>
+        <ul className="product-list">
+          {this.props.products.map(product => {
+            if (
+              !search ||
+              (search &&
+                product.name
+                  .toLocaleLowerCase()
+                  .indexOf(search.toLocaleLowerCase()) > -1)
+            ) {
+              return (
+                <li key={product.id} className="product-list-item">
+                  <Link to={`/products/${product.id}`}>{product.name}</Link>
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (store: IApplicationState) => {
+    return {
+        loading: store.products.productsLoading,
+        products: store.products.products
+    };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        getProducts: () => dispatch(getProducts())
+    };
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsPage);
+

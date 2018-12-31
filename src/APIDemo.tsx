16db1644c1d57908cc6f7a702361b865d6e6950c
chengzh2008 <>
2018-12-31 10:58:33 -0800

    switch to functional component for axios usage

--- src/APIDemo.tsx
+++ src/APIDemo.tsx
@@ -9,117 +9,49 @@ interface IPost {
   body: string;
 }
 
-interface IState {
-  posts: IPost[];
-  error: string;
-  editPost: IPost;
-  cancelTokenSource?: CancelTokenSource;
-  loading: boolean;
-}
+const defaultPosts: IPost[] = [];
 
-class APIDemo extends React.Component<{}, IState> {
-  public constructor(props: {}) {
-    super(props);
-    this.state = {
-      posts: [],
-      error: "",
-      editPost: {
-        body: "",
-        title: "",
-        userId: 1
-      },
-      loading: true
-    };
-  }
+const APIDemo: React.SFC = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  );
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ""
+  );
+  const cancelToken = axios.CancelToken;
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState(true);
+  const [editPost, setEditPost]: [
+    IPost,
+    (post: IPost) => void
+  ] = React.useState({
+    body: "",
+    title: "",
+    userId: 1
+  });
 
-  public componentDidMount() {
-    const cancelToken = axios.CancelToken;
-    const cancelTokenSource = cancelToken.source();
-    this.setState({ cancelTokenSource });
-    axios
-      .get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        cancelToken: cancelTokenSource.token,
-        timeout: 5000
-      })
-      .then(response => {
-        console.log(response);
-        this.setState({ posts: response.data, loading: false });
-      })
-      .catch(ex => {
-        const error = axios.isCancel(ex)
-          ? "Request cancelled"
-          : ex.code === "ECONNABORTED"
-          ? "A timeout has occurred"
-          : ex.response.status === 404
-          ? "Resource not found"
-          : "An unexpected error has occurred";
-        this.setState({ error, loading: false });
-      });
-  }
-
-  public render() {
-    return (
-      <div className="APIDemo">
-        <div className="post-edit">
-          <input
-            type="text"
-            placeholder="Enter title"
-            value={this.state.editPost.title}
-            onChange={this.handleTitleChange}
-          />
-          <textarea
-            placeholder="Enter body"
-            value={this.state.editPost.body}
-            onChange={this.handleBodyChange}
-          />
-          <button onClick={this.handleSaveClick}>Save</button>
-        </div>
-        {this.state.loading && (
-          <button onClick={this.handleCancelClick}>Cancel</button>
-        )}
-        <ul className="posts">
-          {this.state.posts.map(post => (
-            <li key={post.id}>
-              <h3>{post.title}</h3>
-              <p>{post.body}</p>
-              <button onClick={() => this.handleUpdateClick(post)}>
-                Update
-              </button>
-              <button onClick={() => this.handleDeleteClick(post)}>
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
-        {this.state.error && <p className="error">{this.state.error}</p>}
-      </div>
-    );
-  }
-
-  private handleCancelClick() {
-    if (this.state.cancelTokenSource) {
-      this.state.cancelTokenSource.cancel("User cancelled operation");
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel("User cancelled operation");
     }
-  }
-
-  private handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      editPost: { ...this.state.editPost, title: e.currentTarget.value }
-    });
   };
 
-  private handleBodyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({
-      editPost: { ...this.state.editPost, body: e.currentTarget.value }
-    });
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditPost({ ...editPost, title: e.currentTarget.value });
   };
 
-  private handleSaveClick = () => {
-    const editPost = this.state.editPost;
-    if (this.state.editPost.id) {
+  const handleBodyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditPost({ ...editPost, body: e.currentTarget.value });
+  };
+
+  const handleSaveClick = () => {
+    if (editPost.id) {
       axios
         .put<IPost>(
           `https//jsonplaceholder.typicode.com/posts/${editPost.id}`,
@@ -131,16 +63,14 @@ class APIDemo extends React.Component<{}, IState> {
           }
         )
         .then(() => {
-          this.setState({
-            editPost: {
-              body: "",
-              title: "",
-              userId: 1
-            },
-            posts: this.state.posts
-              .filter(post => post.id !== editPost.id)
-              .concat(editPost)
+          setEditPost({
+            body: "",
+            title: "",
+            userId: 1
           });
+          setPosts(
+            posts.filter(post => post.id !== editPost.id).concat(editPost)
+          );
         });
     } else {
       axios
@@ -150,28 +80,79 @@ class APIDemo extends React.Component<{}, IState> {
           }
         })
         .then(response => {
-          this.setState({
-            posts: this.state.posts.concat(response.data)
-          });
+          setPosts(posts.concat(response.data));
         });
     }
   };
 
-  private handleUpdateClick = (post: IPost) => {
-    this.setState({
-      editPost: post
-    });
+  const handleUpdateClick = (post: IPost) => {
+    setEditPost(post);
   };
 
-  private handleDeleteClick = (post: IPost) => {
+  const handleDeleteClick = (post: IPost) => {
     axios
       .delete(`https://jsonplaceholder.typicode.com/posts/${post.id}`)
       .then(() => {
-        this.setState({
-          posts: this.state.posts.filter(p => p.id !== post.id)
-        });
+        setPosts(posts.filter(p => p.id !== post.id));
       });
   };
-}
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        timeout: 5000
+      })
+      .then(response => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch(ex => {
+        const err = axios.isCancel(ex)
+          ? "Request cancelled"
+          : ex.code === "ECONNABORTED"
+          ? "A timeout has occurred"
+          : ex.response.status === 404
+          ? "Resource not found"
+          : "An unexpected error has occurred";
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="APIDemo">
+      <div className="post-edit">
+        <input
+          type="text"
+          placeholder="Enter title"
+          value={editPost.title}
+          onChange={handleTitleChange}
+        />
+        <textarea
+          placeholder="Enter body"
+          value={editPost.body}
+          onChange={handleBodyChange}
+        />
+        <button onClick={handleSaveClick}>Save</button>
+      </div>
+      {loading && <button onClick={handleCancelClick}>Cancel</button>}
+      <ul className="posts">
+        {posts.map(post => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <button onClick={() => handleUpdateClick(post)}>Update</button>
+            <button onClick={() => handleDeleteClick(post)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
 
 export default APIDemo;
